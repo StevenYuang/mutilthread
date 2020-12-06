c4a6cca9e263850264085282c6433ecb4ac0061e
yuangang3 <yuangang3@jd.com>
2020-12-06 12:31:47 +0800

    add project

---
+++ src/test/java/com/yuang/mutilthread/MutilthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuang.mutilthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutilthreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
